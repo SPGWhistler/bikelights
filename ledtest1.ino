1305f241bbeb76784c985d025587a4ecbce07058
Tony Petty <tpetty@synacor.com>
2020-10-04 09:59:21 -0400

    breaking code apart into files

--- ledtest1.ino
+++ ledtest1.ino
@@ -66,6 +66,9 @@ unsigned long tlPreviousMillis = 0;
 unsigned int tlDelay = 50;
 unsigned long slPreviousMillis = 0;
 unsigned int slDelay = 50;
+unsigned long testPreviousMillis = 0;
+unsigned int testDelay = 1000;
+unsigned int testStrip = 0;
 
 void setup() {
   for (int i = 0; i < (sizeof(buttons) / sizeof(buttons[0])); i++) {
@@ -81,17 +84,31 @@ void setup() {
   FastLED.addLeds<WS2812, mainLedPin, GRB>(strip1, 92);
   FastLED.addLeds<WS2812, leftFrontLedPin, GRB>(strip2, 15);
   FastLED.addLeds<WS2812, rightFrontLedPin, GRB>(strip3, 15);
-  //FastLED.setMaxPowerInMilliWatts(500);
+  FastLED.setMaxPowerInMilliWatts(500);
   allOff();
   FastLED.show();
   delay(3000);
 }
 
-// List of patterns to cycle through.  Each is defined as a separate function below.
-typedef void (*SimplePatternList[])(stripProps &strip);
-SimplePatternList gPatterns = { off, rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm };
-uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
-uint8_t gHue = 0; // rotating "base color" used by many of the patterns
+void doTest() {
+  if (currentMillis - testPreviousMillis >= testDelay) {
+    off(strips[0]);
+    off(strips[1]);
+    off(strips[2]);
+    off(strips[3]);
+    off(strips[4]);
+    off(strips[5]);
+    off(strips[6]);
+    solidColor(strips[testStrip], CRGB::Blue);
+    testStrip++;
+    if (testStrip > 6) {
+      testStrip = 0;
+    }
+    testPreviousMillis = currentMillis;
+  }
+}
+
+
 
 
 void loop() {
@@ -99,363 +116,4 @@ void loop() {
   readButtons();
   doAnimationLoop();
   doHorn();
-}
-
-void doHorn() {
-  if (hornOn) {
-    tone(hornPin, 1000);
-  } else {
-    noTone(hornPin);
-  }
-}
-
-void readButtons() {
-  for (int i = 0; i < (sizeof(buttons) / sizeof(buttons[0])); i++) {
-    if (currentMillis - buttons[i].previousMillis >= debounceDelay) {
-      //Debounce time has passed, check state
-      buttons[i].currentState = digitalRead(buttons[i].pin);
-      if (buttons[i].currentState == LOW && buttons[i].previousState == HIGH) {
-        //Button is pressed and was not pressed before
-        switch (i) {
-          case 0: //brakes
-            brakesOn = true;
-            break;
-          case 1: //left blinker
-            leftBlinkerOn = true;
-            break;
-          case 2: //right blinker
-            rightBlinkerOn = true;
-            break;
-          case 3: //horn
-            hornOn = true;
-            break;
-          case 4: //toggle pattern
-            nextPattern();
-            break;
-          case 5: //headlight
-            headLightOn = true;
-            digitalWrite(headLightButtonLed, HIGH);
-            break;
-          case 6: //?
-            break;
-        }
-      } else if (buttons[i].currentState == HIGH && buttons[i].previousState == LOW) {
-        //Button is not pressed and was pressed before
-        switch (i) {
-          case 0: //brakes
-            brakesOn = false;
-            break;
-          case 1: //left blinker
-            leftBlinkerOn = false;
-            digitalWrite(leftBlinkerButtonLed, LOW);
-            break;
-          case 2: //right blinker
-            rightBlinkerOn = false;
-            digitalWrite(rightBlinkerButtonLed, LOW);
-            break;
-          case 3: //horn
-            hornOn = false;
-            break;
-          case 4: //toggle pattern
-            break;
-          case 5: //headlight
-            headLightOn = false;
-            digitalWrite(headLightButtonLed, LOW);
-            break;
-          case 6: //?
-            break;
-        }
-      } //else button was in same state for multiple reads, ignore
-      buttons[i].previousMillis = currentMillis;
-      buttons[i].previousState = buttons[i].currentState;
-    }
-  }
-}
-
-void doAnimationLoop() {
-  updated = false;
-  /*
-  First, do high priority functions, in order of least to highest:
-  Any strips written to by these functions should be locked.
-  But, these functions must not obey that lock.
-  */
-  if (currentMillis - hpPreviousMillis >= hpDelay) {
-    updated = true;
-    doHeadLightAnimation();
-    //doBrakesAnimation();
-    doBlinkerAnimation();
-    hpPreviousMillis = currentMillis;
-  }
-  /*
-  Then call animation functions.
-  These must check each strip's locked state before writing to it.
-  These must obey that lock.
-  */
-  //doGroundEffectAnimation();
-  doTailLightsAnimation();
-  //doSideLightsAnimation();
-  if (updated) {
-    FastLED.show();
-  }
-}
-
-void doGroundEffectAnimation() {
-  if (currentMillis - gfPreviousMillis >= gfDelay && strips[0].writable) {
-    // Call the current pattern function once, updating the 'leds' array
-    gPatterns[gCurrentPatternNumber](strips[0]);
-    if (gCurrentPatternNumber == 0) {
-      digitalWrite(patternToggleButtonLed, LOW);
-    } else {
-      digitalWrite(patternToggleButtonLed, HIGH);
-    }
-    // do some periodic updates
-    EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
-    //EVERY_N_SECONDS( 10 ) { nextPattern(); } // change patterns periodically
-    gfPreviousMillis = currentMillis;
-  }
-}
-
-void doTailLightsAnimation() {
-  if (currentMillis - tlPreviousMillis >= tlDelay) {
-    if (strips[2].writable && strips[3].writable) {
-      //We can do animation...
-      fadeToBlackBy( strips[2].strip, strips[2].count, 40);
-      int pos = beatsin16( 13, strips[2].startIndex, strips[2].startIndex + strips[2].count - 1);
-      strips[2].strip[pos] += CRGB::Red;
-      fadeToBlackBy( strips[3].strip, strips[3].count, 40);
-      int pos2 = beatsin16( 13, strips[3].startIndex, strips[3].startIndex + strips[3].count - 1 );
-      strips[3].strip[pos2] += CRGB::Red;
-    } else {
-      //If we cant do animation, black out any strips that are still writable.
-      if (strips[2].writable) {
-        solidColor(strips[2], CRGB::Black);
-      } else if (strips[3].writable) {
-        solidColor(strips[3], CRGB::Black);
-      }
-    }
-    tlPreviousMillis = currentMillis;
-  }
-}
-
-void doSideLightsAnimation() {
-  if (currentMillis - slPreviousMillis >= slDelay) {
-    if (strips[1].writable && strips[4].writable) {
-      //We can do animation...
-      gPatterns[gCurrentPatternNumber](strips[1]);
-      gPatterns[gCurrentPatternNumber](strips[4]);
-    } else {
-      //If we cant do animation, black out any strips that are still writable.
-      if (strips[1].writable) {
-        solidColor(strips[1], CRGB::Black);
-      } else if (strips[4].writable) {
-        solidColor(strips[4], CRGB::Black);
-      }
-    }
-    slPreviousMillis = currentMillis;
-  }
-}
-
-void doHeadLightAnimation() {
-  if (headLightOn == true) {
-    //Turn headlight on
-    solidColor(strips[5], CRGB::White);
-    solidColor(strips[6], CRGB::White);
-    strips[5].writable = false;
-    strips[6].writable = false;
-  } else if (headLightOn == false && strips[5].writable == false) {
-    //Turn headlight off (should only execute one time)
-    solidColor(strips[5], CRGB::Black);
-    solidColor(strips[6], CRGB::Black);
-    strips[5].writable = true;
-    strips[6].writable = true;
-  }
-}
-
-//Set the back leds to all red
-void doBrakesAnimation() {
-  if (brakesOn == true) {
-    //Turn brakes on
-    if (leftBlinkerOn == false) {
-      solidColor(strips[1], CRGB::Red);
-      solidColor(strips[2], CRGB::Red);
-      strips[1].writable = false;
-      strips[2].writable = false;
-    }
-    if (rightBlinkerOn == false) {
-      solidColor(strips[3], CRGB::Red);
-      solidColor(strips[4], CRGB::Red);
-      strips[3].writable = false;
-      strips[4].writable = false;
-    }
-  } else if (brakesOn == false && strips[1].writable == false) {
-    //TODO This seems like a bug - if the blinker sets writable to false, we'd enter this condition...
-    //Turn brakes off
-    if (leftBlinkerOn == false) {
-      solidColor(strips[1], CRGB::Black);
-      solidColor(strips[2], CRGB::Black);
-      strips[1].writable = true;
-      strips[2].writable = true;
-    }
-    if (rightBlinkerOn == false) {
-      solidColor(strips[3], CRGB::Black);
-      solidColor(strips[4], CRGB::Black);
-      strips[3].writable = true;
-      strips[4].writable = true;
-    }
-  }
-}
-
-void doBlinkerAnimation() {
-  if (currentMillis - blinkerPreviousMillis >= blinkerDelay) {
-    if (leftBlinkerOn == true) {
-      strips[1].writable = false;
-      strips[2].writable = false;
-      if (blink) {
-        solidColor(strips[1], CRGB::Red);
-        solidColor(strips[2], CRGB::Red);
-        digitalWrite(leftBlinkerButtonLed, HIGH);
-      } else {
-        solidColor(strips[1], CRGB::Black);
-        solidColor(strips[2], CRGB::Black);
-        digitalWrite(leftBlinkerButtonLed, LOW);
-      }
-    } else if (leftBlinkerOn == false && strips[1].writable == false && brakesOn == false) {
-      solidColor(strips[1], CRGB::Black);
-      solidColor(strips[2], CRGB::Black);
-      strips[1].writable = true;
-      strips[2].writable = true;
-      digitalWrite(leftBlinkerButtonLed, LOW);
-    }
-    if (rightBlinkerOn == true) {
-      strips[3].writable = false;
-      strips[4].writable = false;
-      if (blink) {
-        solidColor(strips[3], CRGB::Red);
-        solidColor(strips[4], CRGB::Red);
-        digitalWrite(rightBlinkerButtonLed, HIGH);
-      } else {
-        solidColor(strips[3], CRGB::Black);
-        solidColor(strips[4], CRGB::Black);
-        digitalWrite(rightBlinkerButtonLed, LOW);
-      }
-    } else if (rightBlinkerOn == false && strips[3].writable == false && brakesOn == false) {
-      solidColor(strips[3], CRGB::Black);
-      solidColor(strips[4], CRGB::Black);
-      strips[3].writable = true;
-      strips[4].writable = true;
-      digitalWrite(rightBlinkerButtonLed, LOW);
-    }
-    blink = !blink;
-    blinkerPreviousMillis = currentMillis;
-  }
-}
-
-/*
-void fadeOff(int strip[]) {
-  int colorArray = strip[0];
-  int ledStart = strip[1];
-  int ledStop = strip[2];
-  for (int i = ledStart; i <= ledStop; i++) {
-    colors[colorArray][i].fadeToBlackBy(10);
-  }
-  FastLED.show();
-}
-
-void fadeAllOff() {
-  for (int i = 0; i < (sizeof(strips) / sizeof(strips[0])); i++) {
-    int colorArray = strips[i][0];
-    int ledStart = strips[i][1];
-    int ledStop = strips[i][2];
-    for (int j = ledStart; j <= ledStop; j++) {
-      colors[colorArray][j].fadeToBlackBy(10);
-    }
-  }
-  FastLED.show();
-}
-*/
-
-//Set all leds to black
-void allOff() {
-  for (int i = 0; i < (sizeof(strips) / sizeof(strips[0])); i++) {
-    solidColor(strips[i], CRGB::Black);
-  }
-}
-
-//Given an led strip, set all of its pixels to the given color
-void solidColor(stripProps &stripProps, CRGB color) {
-  for (int i = stripProps.startIndex; i < stripProps.startIndex + stripProps.count - 1; i++) { //TODO add count direction
-    stripProps.strip[i] = color;
-  } 
-}
-
-
-
-#define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
-
-void nextPattern()
-{
-  // add one to the current pattern number, and wrap around at the end
-  gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
-}
-
-void off(stripProps &strip)
-{
-  solidColor(strip, CRGB::Black);
-}
-
-void rainbow(stripProps &strip) 
-{
-  // FastLED's built-in rainbow generator
-  fill_rainbow( strip.strip, strip.count, gHue, 7);
-}
-
-void rainbowWithGlitter(stripProps &strip) 
-{
-  // built-in FastLED rainbow, plus some random sparkly glitter
-  rainbow(strip);
-  addGlitter(strip, 80);
-}
-
-void addGlitter( stripProps &strip, fract8 chanceOfGlitter) 
-{
-  if( random8() < chanceOfGlitter) {
-    strip.strip[ random16(strip.startIndex, strip.startIndex + strip.count - 1) ] += CRGB::White;
-  }
-}
-
-void confetti(stripProps &strip) 
-{
-  // random colored speckles that blink in and fade smoothly
-  fadeToBlackBy( strip.strip, strip.count, 10);
-  int pos = random16(strip.startIndex, strip.startIndex + strip.count - 1);
-  strip.strip[pos] += CHSV( gHue + random8(64), 200, 255);
-}
-
-void sinelon(stripProps &strip)
-{
-  // a colored dot sweeping back and forth, with fading trails
-  fadeToBlackBy( strip.strip, strip.count, 20);
-  int pos = beatsin16( 13, strip.startIndex, strip.startIndex + strip.count - 1 );
-  strip.strip[pos] += CHSV( gHue, 255, 192);
-}
-
-void bpm(stripProps &strip)
-{
-  // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
-  uint8_t BeatsPerMinute = 62;
-  CRGBPalette16 palette = PartyColors_p;
-  uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
-  for( int i = 0; i < strip.startIndex + strip.count - 1; i++) { //9948
-    strip.strip[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
-  }
-}
-
-void juggle(stripProps &strip) {
-  // eight colored dots, weaving in and out of sync with each other
-  fadeToBlackBy( strip.strip, strip.count, 20);
-  byte dothue = 0;
-  for( int i = 0; i < 8; i++) {
-    strip.strip[beatsin16( i+7, strip.startIndex, strip.startIndex + strip.count - 1 )] |= CHSV(dothue, 200, 255);
-    dothue += 32;
-  }
 }
